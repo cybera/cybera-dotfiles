420695e8ac7109f16da13697ed345bded71e6382
SnakesAndLadders <skayotte@ayotte-consulting.net>
2019-10-16 22:38:33 +0000

    Changed the structure to make it easier for multiple people

--- create.sh
+++ create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#Modified script from Joe to symlink in dotfiles and keep Git happy using it's own directory.
-
+# Modified script from Joe to symlink in dotfiles and keep Git happy using it's own directory.
+# Shawn ToDo: update README to include usage instructions
 ########## Variables
 
 dir=~/cybera-dotfiles
